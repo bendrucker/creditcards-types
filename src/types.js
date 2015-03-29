ee2120f609d72eb75f9545d3524b0422f98ade97
Ben Drucker <bvdrucker@gmail.com>
2015-03-29 19:42:31 -0400

    Group Diner's Club cards as 4-6-4

--- src/types.js
+++ src/types.js
@@ -39,7 +39,8 @@ exports.americanExpress = new Type('American Express', {
 
 exports.dinersClub = new Type('Diners Club', {
   pattern: /^3(0[0-5]|[68]\d)\d{11}$/,
-  eagerPattern: /^3(0|[68])/
+  eagerPattern: /^3(0|[68])/,
+  groupPattern: /(\d{1,4})?(\d{1,6})?(\d{1,4})?/
 });
 
 exports.discover = new Type('Discover', {
