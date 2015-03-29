e971127a7e09b9840daf01a63f9c82a7f6330ea9
Ben Drucker <bvdrucker@gmail.com>
2015-03-29 17:55:53 -0400

    Remove all non-capturing groups

--- src/types.js
+++ src/types.js
@@ -5,7 +5,7 @@ var Type = require('./type');
 var group19 = /(\d{1,4})(\d{1,4})?(\d{1,4})?(\d{1,4})?(\d{1,3})?/;
 
 exports.visa = new Type('Visa', {
-  pattern: /^4[0-9]{12}(?:[0-9]{3})?$/,
+  pattern: /^4[0-9]{12}([0-9]{3})?$/,
   eagerPattern: /^4/,
   cvcLength: 3
 });
@@ -31,13 +31,13 @@ exports.americanExpress = new Type('American Express', {
 });
 
 exports.dinersClub = new Type('Diners Club', {
-  pattern: /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/,
-  eagerPattern: /^3(?:0|[68])/,
+  pattern: /^3(0[0-5]|[68][0-9])[0-9]{11}$/,
+  eagerPattern: /^3(0|[68])/,
   cvcLength: 3
 });
 
 exports.discover = new Type('Discover', {
-  pattern: /^6(?:011|5[0-9]{2})[0-9]{12}$/,
+  pattern: /^6(011|5[0-9]{2})[0-9]{12}$/,
   eagerPattern: /^6[045]/,
   cvcLength: 3
 });
