9411bbc60c18d3b56f3dc48fc71b95c46fc8a6e7
Ben Drucker <bvdrucker@gmail.com>
2015-06-02 09:42:24 -0400

    Export @ top

--- src/type.js
+++ src/type.js
@@ -2,6 +2,8 @@
 
 var extend = require('xtend/mutable')
 
+module.exports = CardType
+
 function CardType (name, config) {
   extend(this, {name: name}, config)
 }
@@ -21,8 +23,6 @@ CardType.prototype.group = function (number) {
 CardType.prototype.test = function (number, eager) {
   return this[eager ? 'eagerPattern' : 'pattern'].test(number)
 }
-
-module.exports = CardType
 
 function identity (value) {
   return value
