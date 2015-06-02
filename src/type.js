1d9a9a8d7512e57e8328a188bbe8751e94a91244
Ben Drucker <bvdrucker@gmail.com>
2015-06-02 09:43:25 -0400

    Use .filter(Boolean) for removing falsies

--- src/type.js
+++ src/type.js
@@ -9,21 +9,15 @@ function CardType (name, config) {
 }
 
 CardType.prototype.cvcLength = 3
-
 CardType.prototype.luhn = true
-
 CardType.prototype.groupPattern = /(\d{1,4})(\d{1,4})?(\d{1,4})?(\d{1,4})?/
 
 CardType.prototype.group = function (number) {
   return (number.match(this.groupPattern) || [])
     .slice(1)
-    .filter(identity)
+    .filter(Boolean)
 }
 
 CardType.prototype.test = function (number, eager) {
   return this[eager ? 'eagerPattern' : 'pattern'].test(number)
-}
-
-function identity (value) {
-  return value
 }
