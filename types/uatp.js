27a985c889f0815b34b2ebd82d8cb99db8ea9834
Subpixel <subpx@users.noreply.github.com>
2018-01-25 10:59:50 +1100

    Remove cvc from UATP type

--- types/uatp.js
+++ types/uatp.js
@@ -7,6 +7,5 @@ module.exports = Type({
   digits: 15,
   pattern: /^1\d{14}$/,
   eagerPattern: /^1/,
-  groupPattern: /(1\d{2})(\d{1,4})?(\d{1,4})?(\d{1,4})?/,
-  cvcLength: 4
+  groupPattern: /(1\d{2})(\d{1,4})?(\d{1,4})?(\d{1,4})?/
 })
