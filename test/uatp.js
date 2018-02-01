01bda87d013488283add0b87a110d27d1470b016
Thomas Hynard <tomhynard@gmail.com>
2018-02-01 15:19:23 +1100

    Update tests

--- test/uatp.js
+++ test/uatp.js
@@ -9,13 +9,12 @@ test('UATP', function (t) {
   eagerType(t, uatp, '1')
   t.test('Grouping', function (t) {
     t.deepEqual(uatp.group('181529834959453'), [
-      '181',
-      '5298',
-      '3495',
-      '9453'
+      '1815',
+      '29834',
+      '959453'
     ], 'full number')
     t.deepEqual(uatp.group('181'), ['181'], 'partial number')
-    t.deepEqual(uatp.group('18152'), ['181', '52'], 'partial group')
+    t.deepEqual(uatp.group('181529'), ['1815', '29'], 'partial group')
     t.deepEqual(uatp.group(''), [], 'no valid groups')
     t.end()
   })
