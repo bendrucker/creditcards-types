c750df3858021cbfdcddac087efce34758762c3d
Subpixel <subpx@users.noreply.github.com>
2018-03-09 11:28:33 +1100

    Add UATP card type (#37)

---
+++ test/uatp.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var test = require('tape')
+var eagerType = require('./eager-type')
+var uatp = require('../types/uatp')
+
+test('UATP', function (t) {
+  t.ok(uatp.test('181529834959453'), 'normal')
+  eagerType(t, uatp, '1')
+  t.test('Grouping', function (t) {
+    t.deepEqual(uatp.group('181529834959453'), [
+      '1815',
+      '29834',
+      '959453'
+    ], 'full number')
+    t.deepEqual(uatp.group('181'), ['181'], 'partial number')
+    t.deepEqual(uatp.group('181529'), ['1815', '29'], 'partial group')
+    t.deepEqual(uatp.group(''), [], 'no valid groups')
+    t.end()
+  })
+  t.end()
+})
---
+++ types/uatp.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var Type = require('../type')
+
+module.exports = Type({
+  name: 'UATP',
+  digits: 15,
+  pattern: /^1\d{14}$/,
+  eagerPattern: /^1/,
+  groupPattern: /(\d{1,4})(\d{1,5})?(\d{1,6})?/
+})
