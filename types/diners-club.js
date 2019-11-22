c54a7f9d6156c6224bfa0004ccf49793affd4e78
Ben Drucker <bvdrucker@gmail.com>
2019-11-21 20:53:45 -0800

    Allow Diners Club cards up to 19 digits (#53)
    https://github.com/bendrucker/creditcards-types/blob/master/docs/discover.pdf

--- types/diners-club.js
+++ types/diners-club.js
@@ -4,8 +4,8 @@ var Type = require('../type')
 
 module.exports = Type({
   name: 'Diners Club',
-  digits: 14,
-  pattern: /^3(0[0-5]|[68]\d)\d{11}$/,
+  digits: [14, 19],
+  pattern: /^3(0[0-5]|[68]\d)\d{11,16}$/,
   eagerPattern: /^3(0|[68])/,
-  groupPattern: /(\d{1,4})?(\d{1,6})?(\d{1,4})?/
+  groupPattern: /(\d{1,4})?(\d{1,6})?(\d{1,9})?/
 })
