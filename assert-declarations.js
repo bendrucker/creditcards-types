27dec55a5166a2e9a27ed35a9ece144627107535
Ben Drucker <bvdrucker@gmail.com>
2021-02-12 14:21:15 -0800

    print to stderr

--- assert-declarations.js
+++ assert-declarations.js
@@ -3,6 +3,6 @@ const fs = require('fs')
 fs.readdirSync('./types').forEach((filename) => {
   if (!fs.existsSync(`./types/${filename.replace('.js', '.d.ts')}`)) {
     process.exitCode = 1
-    console.log(`missing declaration file for ${filename}`)
+    console.error(`missing declaration file for ${filename}`)
   }
 })
