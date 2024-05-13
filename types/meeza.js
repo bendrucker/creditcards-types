bb52da86073d223b930ccd1e7a55fb21534497b9
Titoot <82727556+Titoot@users.noreply.github.com>
2024-05-13 22:54:14 +0300

    fix incorrect meeza pattern

--- types/meeza.js
+++ types/meeza.js
@@ -5,6 +5,6 @@ const Type = require('../type')
 module.exports = Type({
   name: 'Meeza',
   digits: 16,
-  pattern: /^5078(03|09|10|11)\d{10}$/,
-  eagerPattern: /^5078(03|09|10|11)/
+  pattern: /^5078(03|08|09|10)\d{10}$/,
+  eagerPattern: /^5078(03|08|09|10)/
 })
